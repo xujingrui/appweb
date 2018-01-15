ef95b9cb7bf96428a6c53b6faadfe7a1047b1c99
xujingrui <xujingrui_xjr@163.com>
2018-01-15 19:02:54 +0800

    更新HOME页面

--- appweb.py
+++ appweb.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         if request.form.get('username') == 'admin':
-            return '登录'
+            return render_template('base.html')
+        else:
+            return '登录失败'
     else:
         return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=80)
