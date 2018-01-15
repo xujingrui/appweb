36c131f3c668f74a19a9349891910a565216c0a2
xujingrui <xujingrui_xjr@163.com>
2018-01-15 17:43:24 +0800

    更新POST提交

--- appweb.py
+++ appweb.py
@@ -1,12 +1,20 @@
+#encoding: utf-8
 from flask import Flask,render_template,url_for
 import config
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(config)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        if request.form.get('username') == 'admin':
+            return '登录'
+    else:
+        return render_template('index.html')
+
 
 
 if __name__ == '__main__':
