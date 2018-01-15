7d0dd375736669f4e601572b1e0dc1f8075ae23b
xujingrui <xujingrui_xjr@163.com>
2018-01-15 12:12:20 +0800

    aa

--- appweb.py
+++ appweb.py
@@ -1,12 +1,13 @@
-from flask import Flask,redirect
+from flask import Flask,render_template,url_for
+import config
 
 app = Flask(__name__)
-
+app.config.from_object(config)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1',port=80)
