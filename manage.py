d716daa5fee9b231f74b8f4eef6716d204358e07
xujingrui <xujingrui_xjr@163.com>
2018-01-16 09:30:30 +0800

    aa

---
+++ manage.py
@@ -0,0 +1,21 @@
+#encoding: utf-8
+
+from flask_migrate import Migrate,MigrateCommand
+from appweb import app
+from flask_script import Manager
+from exts import db
+import config
+#from models import UserModel,QuestionModel,AnswerModel
+
+app.config.from_object(config)
+db.init_app(app)
+
+manager = Manager(app)
+
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+
