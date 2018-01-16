5e76716cd7e7d28d1df9d6a980a13f2c83dde860
xujingrui <xujingrui_xjr@163.com>
2018-01-16 12:10:38 +0800

    添加父子模板

--- appweb.py
+++ appweb.py
@@ -22,5 +22,10 @@ def login():
     else:
         return render_template('login.html')
 
+
+@app.route('/CMDB')
+def CMDB():
+    return render_template('biaoge.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=80)
