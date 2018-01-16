18473f5ef75d9f526650cdaae2493af8bd9afd8e
xujingrui <xujingrui_xjr@163.com>
2018-01-16 11:18:14 +0800

    添加父子模板

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 #encoding: utf-8
 
 from exts import db
+import shortuuid
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.String(100),primary_key=True,default=shortuuid.uuid)
+    username = db.Column(db.String(100),nullable=False)
+    telephone = db.Column(db.String(11),nullable=False)
+    _password = db.Column(db.String(100),nullable=False)
